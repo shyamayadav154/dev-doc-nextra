75de2ee077cc87ae1e670266e33d9210b3293872
Shyamanand Yadav <shyamayadav154@gmail.com>
2024-01-11 16:25:19 +0530

    base path change

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
     defaultShowCopyCode:true,
+    basepath: '/',
 })
 
 module.exports = withNextra()
